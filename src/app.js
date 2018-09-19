b9b0f4b33af01b6ca9bd7e5870f55e192aba762b
lorne <2270333671@qq.com>
2018-09-19 17:53:31 +0800

    添加启动页

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {Router} from 'react-native-router-flux'
 import {Scenes} from './config/Scenes'
 import Route from './config/Router'
-
+import SplashScreen from 'react-native-splash-screen'
 
 export default class App extends Component{
 
@@ -11,7 +11,14 @@ export default class App extends Component{
         global.router = this.router;
         global.loginUser = null;
 
-    };
+    }
+
+    componentDidMount() {
+
+        // do stuff while splash screen is shown
+        // After having done stuff (such as async tasks) hide the splash screen
+        SplashScreen.hide()
+    }
 
     render() {
         return   <Router>
